0201b131053686e343e38ebdfca0b6b81f69a2a4
yasahmed <eessaouira@gmail.com>
2019-05-25 10:44:15 +0000

    eee

--- account/src/main/java/com/socle/account/MainAccount.java
+++ account/src/main/java/com/socle/account/MainAccount.java
@@ -1,16 +1,21 @@
 package com.socle.account;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @ComponentScan(value="com.socle.*")
 @EnableFeignClients(basePackages = {"com.socle.account"})
 
 public class MainAccount {
+
     public static void main(String[] args) {
         SpringApplication.run(MainAccount.class, args);
     }
---
+++ account/src/main/java/com/socle/account/configs/H2ConfigServer.java
@@ -0,0 +1,16 @@
+package com.socle.account.configs;
+
+import java.sql.SQLException;
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//@ComponentScan("com.socle.account")
+public class H2ConfigServer {
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
+    }
+}
